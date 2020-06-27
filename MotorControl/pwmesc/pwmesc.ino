4dc8ebc53225f758c80a916cb25d50555c9d9c50
Srikar Gouru <srikarg89@gmail.com>
2020-06-27 17:52:31 -0400

    Arduino ino code

--- MotorControl/pwmesc/pwmesc.ino
+++ MotorControl/pwmesc/pwmesc.ino
@@ -4,7 +4,7 @@ Servo motor;
 
 const int MIN_VAL = 1000;
 const int MAX_VAL = 2000;
-const int TEST_VAL = 1200;
+const int TEST_VAL = 1100;
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,8 +12,8 @@ void setup() {
 
   Serial.begin(9600);
   Serial.println("Arming");
-//  arm();
-  calibrate();
+  arm();
+//  calibrate();
   delay(2000);
   Serial.println("Go for it");
   motor.writeMicroseconds(MIN_VAL);
@@ -45,6 +45,15 @@ void arm(){
   motor.writeMicroseconds(MIN_VAL);
 }
 
+void test(){
+  int sped = 1200;
+  motor.writeMicroseconds(TEST_VAL);
+  Serial.println("Waiting");
+  delay(250);
+  Serial.println("Done waiting");
+  motor.writeMicroseconds(MIN_VAL);
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
 //  Serial.println("Go for it");
@@ -53,9 +62,12 @@ void loop() {
     if(c < 49 || c > 57){
       continue;
     }
+    test();
+    /*
     int sped = map(c, 49, 57, 1000, 2000);
     Serial.println(sped);
     motor.writeMicroseconds(sped);
+    */
   }
 //  motor.writeMicroseconds(TEST_VAL);
 //  Serial.println("Testing");
