352dfba8c9252ad686a24bf06fd30243e017281f
Srikar Gouru <srikarg89@gmail.com>
2020-07-01 12:19:59 -0400

    Use given mil_per_cycle

--- src/mcl/ClockManager.cpp
+++ src/mcl/ClockManager.cpp
@@ -17,7 +17,7 @@ void ClockManager::initialize(int mil_per_cycle){
 void ClockManager::execute(){
     if(mcl_has_started){
         // Wait for that time to pass
-        while(chrono::steady_clock::now() - last_mcl_start_time < chrono::seconds{1}){}
+        while(chrono::steady_clock::now() - last_mcl_start_time < chrono::milliseconds {this->mil_per_cycle}){}
     }
     mcl_has_started = true;
     last_mcl_start_time = chrono::steady_clock::now();
