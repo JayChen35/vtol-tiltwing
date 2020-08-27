d4d1c2ba70a2c49a4b19629e0f91dd51ca4f1816
Srikar Gouru <srikarg89@gmail.com>
2020-08-27 01:04:35 -0400

    Combine ActorCritic into one and a cpl of other minor changes

--- ppo/ppo_main.py
+++ ppo/ppo_main.py
@@ -12,6 +12,7 @@ Clean PyTorch implementation: https://github.com/JayChen35/RL-Adventure-2/blob/m
 Getting a single file from Github: $ curl -L -O https://github.com/JayChen35/RL-Adventure-2/raw/master/3.ppo.ipynb
 Understanding PPO hyperparameters: https://medium.com/aureliantactics/ppo-hyperparameters-and-ranges-6fc2d29bccbe
 Kernels: https://towardsdatascience.com/weight-initialization-in-neural-networks-a-journey-from-the-basics-to-kaiming-954fb9b47c79
+Short Pytorch implementation: https://github.com/nikhilbarhate99/PPO-PyTorch/blob/master/PPO_continuous.py
 """
 
 import tensorflow as tf
@@ -20,27 +21,25 @@ import tensorflow_probability as tfp
 from tensorflow.keras.layers import Dense
 import numpy as np
 import gym
-import gym_reach_and_avoid
 import os
 from datetime import datetime
 from typing import Tuple
 
 
 # Environment parameters
-ENV_NAME = "ReachAndAvoid-v0"
+ENV_NAME = "LunarLanderContinuous-v2"
 ACT_LOW_BOUND         = -1  # Low bound of the action space
 ACT_HIGH_BOUND        = 1
 NUM_EPISODES          = 750
-PPO_STEPS             = 150 # 500  # Episode length/timestep
+PPO_STEPS             = 250 # 500  # Episode length/timestep
 RENDER                = False
-EVAL_WHILE_TRAIN      = False
-EVAL_EPISODE_INTERVAL = 2
-SAVE_MODEL            = True
+EVAL_WHILE_TRAIN      = True
+EVAL_EPISODE_INTERVAL = 5
+SAVE_MODEL            = False
 SAVE_INTERVAL         = 10
 
 # Hyperparameters
-ACTOR_LR         = 1e-04
-CRITIC_LR        = 1e-04
+LR               = 1e-4
 GAMMA            = 0.99  # Discount factor
 GAE_LAMBDA       = 0.95  # GAE smoothing factor
 MINI_BATCH_SIZE  = 20
@@ -48,55 +47,39 @@ CLIP_EPSILON     = 0.2  # Clippling parameter
 PPO_EPOCHS       = 4  # How much to train on a single batch of experience (PPO is on-policy)
 REWARD_THRESHOLD = 90
 CRITIC_DISCOUNT  = 0.5  # 0.5  # c1 in the paper (Value Function Coefficient)
-ENTROPY_BETA     = 0.05 # 0.01  # c2 in the paper
+ENTROPY_BETA     = 0.01 # 0.01  # c2 in the paper
 STD_INITIAL      = 0.5  # Wide initial standard deviation to help exploration
 STD_DECAY        = 0.95
 STD_MINIMUM      = 0.05
 
 
-class Actor(tf.keras.Model):
-    def __init__(self, obs_space: int, act_space: int, std_init=STD_INITIAL):
-        super(Actor, self).__init__()
+class ActorCritic(tf.keras.Model):
+    def __init__(self, obs_space: int, act_space: int):
+        super().__init__()
         self.observation_space = obs_space
         self.action_space = act_space
-        # The Actor explores the environment and maps states to actions (called the "policy").
+        # Both actor and critic have the same common network
         self.layer_1 = Dense(128, input_shape=(self.observation_space,), kernel_initializer="he_uniform", activation="relu")
         self.layer_2 = Dense(64, kernel_initializer="he_uniform", activation="relu")
-        self.output_layer = Dense(self.action_space, kernel_initializer="glorot_uniform", activation="softplus")
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=ACTOR_LR)
-        # The standard deviations in the action probability distribution.
-        self.std = std_init
+        # The Actor explores the environment and maps states to actions (called the "policy").
+        # The Critic evalutates the Actor's policy and runs stochastic gradient descent on states->rewards.
+        self.actor = Dense(self.action_space, kernel_initializer="glorot_uniform", activation="tanh")
+        # The output of the Critic is a single value (the output of the Value Function).
+        self.critic = Dense(1, kernel_initializer="glorot_uniform", activation=None)
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=LR)
+        self.std = STD_INITIAL
         self.sigma = tf.ones([1, self.action_space], dtype=tf.float64) * self.std
 
-    def call(self, x: np.ndarray or tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
-        x = self.layer_1(x)
-        x = self.layer_2(x)
-        mu = self.output_layer(x)  # The means of the Gaussian distribution is the output of the Actor network
-        return mu, self.sigma
+    def call(self, inputs):
+        x = self.layer_1(inputs)
+        x = self.layer_2(inputs)
+        return self.actor(x), tf.squeeze(self.critic(x))
 
     def update_sigma(self, std_decay=STD_DECAY):
         # Update the standard deviation (random exploration) of the policy. Should be called at the end of an episode
         if self.std > STD_MINIMUM:
             self.std = self.std * std_decay
             self.sigma = tf.ones([1, self.action_space], dtype=tf.float64) * self.std
-
-
-class Critic(tf.keras.Model):
-    def __init__(self, obs_space: int, act_space: int):
-        super(Critic, self).__init__()
-        self.observation_space = obs_space
-        self.action_space = act_space
-        # The Critic evalutates the Actor's policy and runs stochastic gradient descent on states->rewards.
-        self.layer_1 = Dense(128, input_shape=(self.observation_space,), kernel_initializer="he_uniform", activation="relu")
-        self.layer_2 = Dense(64, kernel_initializer="he_uniform", activation="relu")
-        self.output_layer = Dense(1, kernel_initializer="glorot_uniform", activation=None)
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=CRITIC_LR)
-        # The output of the Critic is a single value (the output of the Value Function).
-
-    def call(self, x: np.ndarray or tf.Tensor) -> tf.float64:
-        x = self.layer_1(x)
-        x = self.layer_2(x)
-        return tf.squeeze(self.output_layer(x))
 
 
 class Trajectory:
@@ -114,32 +97,26 @@ class Trajectory:
 
 
 @tf.function
-def train_step(actor: Actor, critic: Critic, trajectory: Trajectory):
+def train_step(model: ActorCritic, trajectory: Trajectory):
     # Perform SGD on both the Actor and Critic with the PPO custom loss function
     with tf.GradientTape() as tape:
-        tape.watch(actor.layer_1.variables)
-        tape.watch(actor.layer_2.variables)
-        tape.watch(actor.output_layer.variables)
-        loss, actor_loss, critic_loss, entropy = ppo_loss(actor, critic, trajectory)
-    gradients = tape.gradient(loss, actor.trainable_variables)
-    actor.optimizer.apply_gradients(zip(gradients, actor.trainable_variables))
-    # Stochastic gradient descent on the Critic
-    with tf.GradientTape() as tape:
-        tape.watch(critic.layer_1.variables)
-        tape.watch(critic.layer_2.variables)
-        tape.watch(critic.output_layer.variables)
-        loss, actor_loss, critic_loss, entropy = ppo_loss(actor, critic, trajectory)
-    # gradients = tape.gradient(loss, critic.trainable_variables)
-    gradients = tape.gradient(critic_loss, critic.trainable_variables)
-    critic.optimizer.apply_gradients(zip(gradients, critic.trainable_variables))
+        tape.watch(model.layer_1.variables)
+        tape.watch(model.layer_2.variables)
+        tape.watch(model.actor.variables)
+        tape.watch(model.critic.variables)
+        loss, actor_loss, critic_loss, entropy = ppo_loss(model, trajectory)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    # print(type(gradients), len(gradients), None in gradients)
+    gradients = [grad if grad is not None else tf.zeros_like(var) for (grad, var) in zip(gradients, model.trainable_variables)]
+    model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     return loss, actor_loss, critic_loss, entropy  # Don't need to return loss, but returning to keep track of statistics
 
 
-def run_actor_critic(actor: Actor, critic: Critic, state: np.ndarray) -> Tuple[tfp.distributions.TruncatedNormal, float]:
+def run_actor_critic(model: ActorCritic, state: np.ndarray) -> Tuple[tfp.distributions.TruncatedNormal, float]:
     # Simply a wrapper to return the intended outputs for the Actor and Critic (Actor can't return a Distribution)
-    mu, sigma = actor(state)
-    dist = tfp.distributions.TruncatedNormal(mu, sigma, ACT_LOW_BOUND, ACT_HIGH_BOUND)
-    value_estimate = critic(state)
+    mu, value_estimate = model(state)
+    # print(mu)
+    dist = tfp.distributions.TruncatedNormal(mu, model.sigma, ACT_LOW_BOUND, ACT_HIGH_BOUND)
     return dist, value_estimate
 
 
@@ -157,11 +134,12 @@ def compute_gae(next_value: tf.Tensor, rewards: list, masks: list, values: list)
     return returns
 
 
-def ppo_loss(actor: Actor, critic: Critic, trajectory: Trajectory):
+def ppo_loss(model: ActorCritic, trajectory: Trajectory):
     state, action, old_log_probs, actual_return, advantage = trajectory.to_tuple()
     # Actual_return = the computed GAE
-    dist, value = run_actor_critic(actor, critic, state)
-    entropy = tf.reduce_mean(dist.entropy())  # reduce_mean() is just the mean
+    dist, value = run_actor_critic(model, state)
+
+
     new_log_probs = dist.log_prob(action)
     # Policy_ratio = r(theta) in the paper, basically how much more likely are we to take an action with this new policy?
     policy_ratio = tf.math.exp(new_log_probs-old_log_probs)  # Expressed as a difference of logs
@@ -170,7 +148,10 @@ def ppo_loss(actor: Actor, critic: Critic, trajectory: Trajectory):
     # Actor loss follows the clipped objective, while Critic loss is just MSE between returns and the Critic prediction
     actor_loss = -1 * tf.reduce_mean(tf.minimum(surr_1, surr_2))
     critic_loss = tf.reduce_mean(tf.pow(actual_return-value, 2))
-    # entropy = tf.reduce_mean(-(new_log_probs * (new_log_probs + 1e-10)))
+
+    # entropy = tf.reduce_mean(dist.entropy())  # reduce_mean() is just the mean
+    entropy = tf.reduce_mean(-(tf.math.exp(new_log_probs) * new_log_probs))
+
     total_loss = CRITIC_DISCOUNT * critic_loss + actor_loss - ENTROPY_BETA * entropy
     return total_loss, actor_loss, critic_loss, entropy
 
@@ -188,7 +169,7 @@ def ppo_minibatch_iterator(states: list, actions: list, log_probs: list, returns
             yield trajectory
             
 
-def ppo_update(actor: Actor, critic: Critic, states: list, actions: list, log_probs: list, returns: list, advantages: list):
+def ppo_update(model: ActorCritic, states: list, actions: list, log_probs: list, returns: list, advantages: list):
     loss_totals = []
     actor_loss_totals = []
     critic_loss_totals = []
@@ -197,7 +178,7 @@ def ppo_update(actor: Actor, critic: Critic, states: list, actions: list, log_pr
     for epoch in range(PPO_EPOCHS):
         # Grabs random mini-batches of trajectories several times until all the data is covered
         for trajectory in ppo_minibatch_iterator(states, actions, log_probs, returns, advantages):
-            total_loss, actor_loss, critic_loss, entropy = train_step(actor, critic, trajectory)
+            total_loss, actor_loss, critic_loss, entropy = train_step(model, trajectory)
             loss_totals.append(total_loss)
             actor_loss_totals.append(actor_loss)
             critic_loss_totals.append(critic_loss)
@@ -205,15 +186,15 @@ def ppo_update(actor: Actor, critic: Critic, states: list, actions: list, log_pr
     return np.mean(loss_totals), np.mean(actor_loss_totals), np.mean(critic_loss_totals), np.mean(entropies)
 
 
-def eval_current_policy(actor: Actor):
-    state = np.reshape(env.reset(), [1, actor.observation_space])
+def eval_current_policy(model: ActorCritic):
+    state = np.reshape(env.reset(), [1, model.observation_space])
     done = False
     total_reward = 0
     step = 0
     while not done and step < PPO_STEPS:
-        mu, sigma = actor(state)
-        dist = tfp.distributions.TruncatedNormal(mu, sigma, ACT_LOW_BOUND, ACT_HIGH_BOUND)
-        next_state, reward, done, _  = env.step(dist.sample()[0])
+        env.render()
+        mu, _ = model(state)
+        next_state, reward, done, _  = env.step(mu[0])
         next_state = np.reshape(next_state, [1, observation_space])
         total_reward += reward
         state = next_state
@@ -241,7 +222,7 @@ if __name__ == "__main__":
     env = gym.make(ENV_NAME)
     observation_space = env.observation_space.shape[0]
     action_space = env.action_space.shape[0]
-    actor, critic = Actor(observation_space, action_space), Critic(observation_space, action_space)
+    model = ActorCritic(observation_space, action_space)
 
     if SAVE_MODEL:
         tensorboard_path, saved_model_path, checkpoint_path = form_results()
@@ -262,7 +243,7 @@ if __name__ == "__main__":
             # This loop collects experiences gathered by the agent (Trajectories)
             if RENDER:
                 env.render()
-            dist, value = run_actor_critic(actor, critic, state)
+            dist, value = run_actor_critic(model, state)
             action = dist.sample()[0]
             next_state, reward, terminal, _ = env.step(action)
             next_state = np.reshape(next_state, [1, observation_space])
@@ -279,30 +260,29 @@ if __name__ == "__main__":
             if terminal:
                 state = np.reshape(env.reset(), [1, observation_space])  # Reset the environment
 
-        next_value = critic(next_state)
+        _, next_value = model(next_state)
         returns = compute_gae(next_value, rewards, masks, values)
         advantages = [tf.math.subtract(returns[x], values[x]) for x in range(len(returns))]
         # advantages = tf.linalg.normalize(advantages)[0]
-        avg_total_loss, avg_actor_loss, avg_critic_loss, avg_entropy = ppo_update(actor, critic, states, actions, log_probs, returns, advantages)
+        avg_total_loss, avg_actor_loss, avg_critic_loss, avg_entropy = ppo_update(model, states, actions, log_probs, returns, advantages)
         print("[Episode {}] Total reward: {}. AVG total loss: {}. AVG Actor loss: {}. AVG Critic loss: {}. AVG entropy: {}. STD: {}.".format(
-            episode, total_reward, avg_total_loss, avg_actor_loss, avg_critic_loss, avg_entropy, actor.std))
+            episode, total_reward, avg_total_loss, avg_actor_loss, avg_critic_loss, avg_entropy, model.std))
         episode += 1
-        actor.update_sigma()  # Decay the STD every episode
+        model.update_sigma()  # Decay the STD every episode
         # Quickly test if the network has reached the threshold reward
         if EVAL_WHILE_TRAIN and episode % EVAL_EPISODE_INTERVAL == 0:
-            test_reward = np.mean([eval_current_policy(actor) for _ in range(5)])
-            test_rewards.append(test_reward)
+            test_reward = np.mean([eval_current_policy(model) for _ in range(5)])
             if test_reward >= REWARD_THRESHOLD: early_stop = True
             if test_reward > max(test_rewards) and SAVE_MODEL:
-                actor.save(saved_model_path + '/Actor_eval', overwrite=True)
-                critic.save(saved_model_path + '/Critic_eval', overwrite=True)
+                model.save(saved_model_path + '/Model_eval', overwrite=True)
                 print("Models saved on Episode {} since reward > previous average reward ({}>{}).".format(
                     episode, test_reward, max(test_rewards)))
             else:
                 print("Models NOT saved since the current reward ({}) is not greater than the max reward ({}).".format(
                     test_reward, max(test_rewards)))
+            test_rewards.append(test_reward)
+            env.close()
         # Save the model
         if SAVE_MODEL and episode % SAVE_INTERVAL == 0:
-            actor.save(saved_model_path + '/Actor_interval', overwrite=True)
-            critic.save(saved_model_path + '/Critic_interval', overwrite=True)
-            print("Actor and Critic models successfully saved on Episode {}.".format(episode))
+            model.save(saved_model_path + '/Model_interval', overwrite=True)
+            print("ActorCritic models successfully saved on Episode {}.".format(episode))
